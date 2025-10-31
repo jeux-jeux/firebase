94172d1348252afe4b42b54086d857a90dee6090
jeux-jeux <danielersen@icloud.com>
2025-10-31 23:26:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,47 @@ allowed = j.get("origine_stockage")
 level = j.get("level")
 port = j.get("port")
 
+#Compteur bits
+data_cache = {
+    "get":[],
+    "get_bits":[],
+    "patch":[],
+    "patch_bits":[],
+    "put":[],
+    "put_bits":[]
+}
+def filtrer(liste):
+    maintenant = time.time()
+    liste_return = liste
+    if len(liste_return) > 0:
+        while liste_return and maintenant - liste_return[0] > 30:
+            liste_return.pop(0)
+        while len(liste_return) > len(data_cache[liste_return + "_bits"]):
+            data_cache[liste_return + "_bits"].pop(0)
+        
+    return liste_return
+def nettoyer_historique():
+    """Supprime les entrées de plus de 30 secondes"""
+    data_cache[get] = filtrer(data_cache[get])
+    data_cache[patch] = filtrer(data_cache[patch])
+    data_cache[put] = filtrer(data_cache[put])
+    
+@app.before_request
+def enregistrer_requete():
+    """Ajoute un horodatage à chaque requête"""
+    nettoyer_historique()
+    def bits_fonction(list):
+        x = 0
+        for item in list:
+            x += item
+        return x
+    bits_json = {
+        "get":bits_fonction(get_bits),
+        "patch":bits_fonction(patch_bits),
+        "put":bits_fonction(put_bits)
+    }
+    
+    
 @app.route('/', defaults={'subpath': ''}, methods=['GET','PUT','DELETE','PATCH'])
 @app.route('/<path:subpath>', methods=['GET','PUT','DELETE','PATCH'])
 def route(subpath):
@@ -52,7 +93,7 @@ def route(subpath):
         if not access == "false":
             ok = True
 
-    if ok == True:
+    if ok == True and not subpath == "stats":
         method = request.method
         data.pop("cle", None)
         if method == 'GET':
@@ -77,8 +118,10 @@ def route(subpath):
             return jsonify(j)
         else:
             return jsonify({ 'error': "Méthode non ajoutée par le développeur."})
-
-
+    elif ok == True and subpath == "stats":
+        return jsonify(bits_json)
+    else:
+        return jsonify({"Message":"Acces refusé"})
 if __name__ == '__main__':
     port = int(port)
     print(f"⚡️ Proxy firebase actif sur le port {port}")
